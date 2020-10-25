285bd5357a4ee06862f92cb14b41af3be359b358
david-rodriguez-levelup <72571654+david-rodriguez-levelup@users.noreply.github.com>
2020-10-25 18:29:18 +0100

    First commit!

--- Assets/Scripts/Comments.cs
+++ Assets/Scripts/Comments.cs
@@ -2,43 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Player manager.
+/// Contains all methods for managing player objects: find, add, remove, etc.
+/// </summary>
+/// <seealso cref="Player"/>
 public class Comments : MonoBehaviour
 {
+    
+    /// Action invoked when a player is added to the list of players.
     public event Action<Player> PlayerAdded = delegate { };
 
+    /// Action invoked when a player is removed from the list of players.
     public event Action<Player> PlayerRemoved = delegate { };
 
+    /// List of players, instantiated on the Awake method.
     private List<Player> _players;
 
+    /// <summary>
+    /// Initializes the list of players.
+    /// </summary>
     private void Awake()
     {
         _players = new List<Player>();
     }
 
+    /// <summary>
+    /// Searches for the player with the given id in the list of players.
+    /// </summary>
+    /// <param name="id">Id of the player.</param>
+    /// <returns>The first player with the given id in the list of players or <c>null</c> otherwise.</returns>
+    /// <seealso cref="Player.Id"/>
     public Player GetPlayerById(int id)
     {
         return _players.Find(player => player.Id == id);
     }
 
+    /// <summary>
+    /// Adds the player to the list of players and calls <c>OnPlayerAdded</c>.
+    /// </summary> 
+    /// <remarks>
+    /// This method does not check if the player to add is already in the list of players and calls <c>OnPlayerAdded</c> anyway.
+    /// </remarks>    
+    /// <param name="player">The player to be added.</param>
+    /// <seealso cref="Player"/>
     public void AddPlayer(Player player)
     {
         _players.Add(player);
-
         OnPlayerAdded(player);
     }
 
+    /// <summary>
+    /// Removes the player from the list of players (if exists) and calls <c>OnPlayerRemoved</c>.
+    /// <remarks>
+    /// This method does not check if the player to remove is in the list of players and calls <c>OnPlayerRemoved</c> anyway.
+    /// </remarks>
+    /// </summary>
+    /// <param name="player">The player to be removed.</param>
+    /// <seealso cref="Player"/>
     public void RemovePlayer(Player player)
     {
         _players.Remove(player);
-
         OnPlayerRemoved(player);
     }
 
+    /// <summary>
+    /// Returns the position of the player's transform in the world.
+    /// </summary>
+    /// <param name="id">Id of the player.</param>
+    /// <returns>A <c>Vector3</c> with the position of the player's transform in the world.</returns>
+    /// <exception cref="System.NullReferenceException">If the player with the given id doesn't exists in the list of players.</exception>
+    /// <seealso cref="Player.Id"/>
     public Vector3 GetPlayerPositionById(int id)
     {
         return GetPlayerById(id).transform.position;
     }
 
+    /// <summary>
+    /// Invokes <c>PlayerAdded</c> action.
+    /// </summary>
+    /// <param name="player">The added player.</param>
+    /// <seealso cref="Player"/>    
     public void OnPlayerAdded(Player player)
     {
         if(PlayerAdded != null)
@@ -47,6 +91,11 @@ public class Comments : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Invokes <c>PlayerRemoved</c> action.
+    /// </summary>
+    /// <param name="player">The removed player.</param>
+    /// <seealso cref="Player"/>    
     public void OnPlayerRemoved(Player player)
     {
         if (PlayerAdded != null)
@@ -54,4 +103,5 @@ public class Comments : MonoBehaviour
             PlayerRemoved.Invoke(player);
         }
     }
+
 }
