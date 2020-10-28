732634dab47afdd75801c58ef37c9e2a04b11364
david-rodriguez-levelup <72571654+david-rodriguez-levelup@users.noreply.github.com>
2020-10-28 19:34:28 +0100

    Update Player.cs
    Comments in Player.cs por si las moscas! :D

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,21 @@
-﻿using UnityEngine;
+﻿///
+/// INFORMATION
+///
+/// Project: Exercise04
+/// Date: 28/10/2020
+/// Author: Level Up
+/// Website: https://www.game-levelup.com/
+/// Programmers: David Cuenca, Pau Elias
+/// Description: Player model.
+///
+
+using UnityEngine;
 
 public class Player: MonoBehaviour
 {
+    /// Name of the player. 
     public string Name { get; private set; }
 
+    /// Id of the player.
     public int Id { get; private set; }
 }
